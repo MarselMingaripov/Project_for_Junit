8b000ac4031c038293d8c2398ad9f8da46c880be
MarselMingaripov <bard1812@gmail.com>
2023-03-09 21:39:39 +0300

    sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -26,5 +26,37 @@ SELECT first_name AS имя, MAX(age) AS возраст FROM employee
 HAVING count(first_name)>1
 ORDER BY возраст DESC;
 
+CREATE TABLE city(
+                     city_id BIGSERIAL NOT NULL PRIMARY KEY,
+                     city_name VARCHAR(60) NOT NULL
+);
+
+ALTER TABLE employee ADD COLUMN city_id BIGINT DEFAULT NULL;
+
+ALTER TABLE employee ADD CONSTRAINT fk_city FOREIGN KEY (city_id) REFERENCES city (city_id);
+
+INSERT INTO city(city_name) VALUES ('Tula');
+INSERT INTO city(city_name) VALUES ('Kazan');
+INSERT INTO city(city_name) VALUES ('Moscow');
+
+UPDATE employee SET city_id = 1 where id = 1;
+UPDATE employee SET city_id = 1 where id = 2;
+UPDATE employee SET city_id = 3 where id = 3;
+UPDATE employee SET city_id = 3 where id = 4;
+UPDATE employee SET city_id = 4 where id = 5;
+
+SELECT city, first_name, last_name FROM city
+LEFT JOIN employee
+ON employee.city_id = city.city_id;
+
+SELECT first_name, city FROM employee
+FULL JOIN city
+ON employee.city_id = city.city_id;
+
+SELECT first_name, city FROM employee
+CROSS JOIN city;
+
+
+
 
 
